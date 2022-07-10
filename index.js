90bfebc6f590abc0dec6fff51e8c0d08efb00337
Maira Silveira <silveirasmaira@gmail.com>
2022-07-10 16:56:31 -0300

    js

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
- 
+const form = document.getElementById('form');
+form.addEventListener('submit', handleSubmit);
+let status = document.getElementById('status');
+let attempt = document.getElementById('attempt');
+let result = document.getElementById('result');
+
+const Guess = {
+    max = 10,
+    attemptsNumber: 0
+    numberDrawn: function randomValue() {
+        retur Math.round(Math.random() * this.max);
+    }
+}
+
+let numberDrawn = Guess.numberDrawn();
+
+function updateAttempt(attempt, value) {
+    attempt.innerHTML = 'Tentativa: ' + value;
+}
+
+function handleSubmit(e) {
+    e.preventDefault(); //para nao recarregar pag quando enviar formulário.
+
+    let kick = document.getElementById('kick').value;
+
+    if (!kick) {
+        alert('Digite um valor');
+        return;
+    }
+
+    updateAttempt(attempt, ++Guess.attemptsNumber);
+
+    if (numberDrawn == kick) {
+        playAgain();
+        status.innerHTML = 'Parabéns, acertou!';
+        result.style.transition = 0.4s;
+        result.style.backgroundColor = '#37c978';
+        result.style.color = '#fff';
+        status.style.color = '#fff';
+        clear(); //para limpar o valor do input
+        else if(numberDrawn > kick) {
+            status.innerHTML = 'O número é maior!';
+            status.style.color = '#de4251';
+            clear();
+        } else if(numberDrawn < kick){
+            status.innerHTML = 'O número é menor!';
+            status.style.color = '#de4251';
+            clear();
+        }
+    }
+}
+
+function playAgain() {
+    document.getElementById('btnRestart').style.display = 'flex';
+}
+
+function restart() {
+    document.location.reload(true);
+}
+
+function clear() {
+    document.getElementById('kick').value = '';
+}
